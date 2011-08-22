087ad48037d993cc38ec053785769b372e0a9532
Billy Cheng Bin <sz.chengbin@gmail.com>
2011-08-22 22:38:00 +0800

    init the project again now

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yueyue::Application.config.secret_token = '760e3ed5d5059952dbf474fb8db7e65bded09bd4f8f6754d2ea86a1d8ea135dd5122415fef5d49002274e461bb7a421fd89baeb46dd25cb82482fac6a1855cfc'
