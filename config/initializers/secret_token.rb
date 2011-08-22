0ea4497bac7f415e51c49c5746bbd3185563843d
Billy Cheng Bin <sz.chengbin@gmail.com>
2011-08-22 22:34:02 +0800

    delete all before re-init the project

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Yueyue::Application.config.secret_token = '6281efebc97bb5cf8bc75316ed5ff43b5b1d7789218201a2c2965fd8cc409fb118285c4dae8de6536153aac12c304ab57883971c243431c2e4537bdc30ff8b85'
