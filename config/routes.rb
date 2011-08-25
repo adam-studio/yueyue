aa0a8320e7c705ecdfd7ac50ce6c87dd8384e000
Billy Cheng Bin <sz.chengbin@gmail.com>
2011-08-26 00:06:11 +0800

    add navigate function

--- config/routes.rb
+++ config/routes.rb
@@ -54,7 +54,7 @@ Yueyue::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "sanguoshas#index"
+  root :to => "topic#index"
 
   # See how all your routes lay out with "rake routes"
 
